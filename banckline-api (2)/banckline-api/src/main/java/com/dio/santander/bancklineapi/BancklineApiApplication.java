f75954466253d403c238ea905cd059e3c94ae032
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-05-04 21:47:19 -0300

    first commit

---
+++ banckline-api (2)/banckline-api/src/main/java/com/dio/santander/bancklineapi/BancklineApiApplication.java
@@ -0,0 +1,15 @@
+package com.dio.santander.bancklineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancklineApiApplication.class, args); 
+		System.out.println("Ola Mundo!!!");
+
+	}
+
+}
---
+++ banckline-api (2)/banckline-api/src/main/java/com/dio/santander/bancklineapi/model/MovimentacaoTipo.java
@@ -0,0 +1,7 @@
+package com.dio.santander.bancklineapi.model;
+
+public enum MovimentacaoTipo {
+	RECEITA,
+	DESPESA
+
+}
